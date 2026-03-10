15b2556a69ec7f72741a68028f3beb9f044313a0
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-10 18:02:38 +0530

    istech

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,18 +1,33 @@
-// This is the chat page where user chats with other user.
 import React from 'react'
-// Componenets 
 import List from '../components/List'
 import Detail from '../components/Detail'
 import Chatbox from '../components/Chatbox'
-import { Chatstore } from '../ChatStore' // Chat Zustand Management
+import { Chatstore } from '../ChatStore'
 
 export default function Chat() {
-  const { chatId } = Chatstore() // Chat Management
+
+  const { chatId } = Chatstore()
+
   return (
-    <div className=' flex flex-row'>
-      <List />
-      {chatId && < Chatbox />}
-      {chatId && <Detail />}
+
+    <div className="flex flex-col md:flex-row h-screen gap-3 p-2">
+
+      {/* Left Panel */}
+      <div className={`${chatId ? "hidden md:block" : "block"} md:w-[30%] lg:w-[25%]`}>
+        <List />
+      </div>
+
+      {/* Middle Panel */}
+      <div className={`${chatId ? "block" : "hidden md:block"} flex-1`}>
+        {chatId && <Chatbox />}
+      </div>
+
+      {/* Right Panel */}
+      <div className="hidden lg:block lg:w-[25%]">
+        {chatId && <Detail />}
+      </div>
+
     </div>
+
   )
 }
