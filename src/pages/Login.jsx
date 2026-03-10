2d5bdcdc46843a754a1dfdb6d25e2ac7a761fe59
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-10 17:58:10 +0530

    istesf

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -44,34 +44,65 @@ export default function Login({ onNavigateToSignup }) { // Pass navigation handl
   };
 
   return (
-    <div className="shadow-lg mt-[70px] text-center bg-gray-900 bg-opacity-80 rounded-xl p-6 border border-gray-500">
-      <div className="text-[25px] text-white font-semibold">Login</div>
-      {/* Input Fields */}
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email Address"
-        className="w-[400px] h-[50px] mt-[10px] border-white rounded-lg px-3 bg-transparent text-white placeholder-gray-300 focus:outline-none"
-        required /><br />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password"
-        className="w-[400px] h-[50px] mt-[10px] border-white rounded-lg px-3 bg-transparent text-white placeholder-gray-300 focus:outline-none"
-        required /><br />
-      {/* Login Button */}
-      <button type="button" onClick={login} className="bg-blue-800 text-white hover:bg-blue-600 rounded-xl h-[50px] w-[400px] mt-[20px]">
-        Login
-      </button>
-      {/* Google Sign In */}
-      <div className="flex flex-col mt-[20px]">
-        <button onClick={googlesignin}
-          className="flex items-center justify-center bg-blue-500 rounded-xl h-[50px] text-white hover:bg-blue-700 w-[400px] space-x-2">
-          <img src={google} alt="google" className="w-9 h-9" />
-          <span>Continue with Google</span>
+    <div className="flex justify-center items-center min-h-screen px-4">
+
+      <div className="shadow-lg text-center bg-gray-900 bg-opacity-80 rounded-xl p-6 border border-gray-500 w-full max-w-md">
+
+        <div className="text-2xl text-white font-semibold mb-4">
+          Login
+        </div>
+
+        {/* Email */}
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter Email Address"
+          className="w-full h-[50px] mt-2 border border-white rounded-lg px-3 bg-transparent text-white placeholder-gray-300 focus:outline-none"
+        />
+
+        {/* Password */}
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter Password"
+          className="w-full h-[50px] mt-3 border border-white rounded-lg px-3 bg-transparent text-white placeholder-gray-300 focus:outline-none"
+        />
+
+        {/* Login Button */}
+        <button
+          type="button"
+          onClick={login}
+          className="bg-blue-800 text-white hover:bg-blue-600 rounded-xl h-[50px] w-full mt-5"
+        >
+          Login
         </button>
+
+        {/* Google Sign In */}
+        <div className="flex flex-col mt-5">
+          <button
+            onClick={googlesignin}
+            className="flex items-center justify-center bg-blue-500 rounded-xl h-[50px] text-white hover:bg-blue-700 w-full space-x-2"
+          >
+            <img src={google} alt="google" className="w-8 h-8" />
+            <span>Continue with Google</span>
+          </button>
+        </div>
+
+        {/* Navigate to Signup */}
+        <div className="text-base text-white mt-6">
+          New to the Platform? <br />
+          <button
+            className="text-white font-semibold hover:font-bold hover:text-black"
+            onClick={onNavigateToSignup}
+          >
+            Click Here
+          </button>
+        </div>
+
       </div>
-      {/* Navigate to SignUp page */}
-      <div className="text-[18px] text-white mt-[20px]">
-        New to the Platform? <br />
-        <button className="text-white font-semibold hover:font-bold hover:text-black" onClick={onNavigateToSignup}>
-          Click Here
-        </button>
-      </div>
+
     </div>
   );
 }
