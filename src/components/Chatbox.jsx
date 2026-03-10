f816e1f96af81880079989bc9cced087d3daeee6
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-10 11:58:06 +0530

    server//

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -12,7 +12,7 @@ import { Userstore } from "../usestore"; // Userstore
 import { Chatstore } from "../ChatStore"; // Chatstore
 import EmojiPicker from "emoji-picker-react"; // Emojis
 import { arrayUnion, onSnapshot, doc, updateDoc } from "firebase/firestore"; // Firebase Firestore
-
+// Google server for internet connection
 const servers = {
   iceServers: [
     {
