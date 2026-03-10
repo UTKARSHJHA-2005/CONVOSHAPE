253fe9b6bc38952a9f2b2a0188b186a67c4303ef
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-10 11:54:54 +0530

    img

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -1,7 +1,7 @@
 // This is the middle component of chat page. 
 import React, { useEffect, useRef, useState } from "react";
 // Images
-import avatar from '../assets/avatar.jpg'; 
+import avatar from '../assets/avatar.jpg';
 import phone from "../assets/phone-call.png";
 import video from "../assets/video.png";
 import mic from "../assets/mic.png";
@@ -16,7 +16,7 @@ import { arrayUnion, onSnapshot, doc, updateDoc } from "firebase/firestore"; // 
 export default function Chatbox() {
   const [text, setText] = useState(""); // Text input state
   const [imageBase64, setImageBase64] = useState(null);  // Image state
-  const {chatId,user,isCurrentUserBlocked,isRecieverBlocked,changeBlock}=Chatstore() // Chat Management
+  const { chatId, user, isCurrentUserBlocked, isRecieverBlocked, changeBlock } = Chatstore() // Chat Management
   const [open, setOpen] = useState(false); // Emoji Picker state
   const { currentUser } = Userstore(); // Current User
   const [Chat, setChat] = useState(); // Chat state
@@ -45,7 +45,7 @@ export default function Chatbox() {
         }),
       });
       setText("");
-      setImageBase64(null); 
+      setImageBase64(null);
     } catch (error) {
       console.error("Error sending message:", error);
     }
@@ -56,7 +56,7 @@ export default function Chatbox() {
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        setImageBase64(reader.result); 
+        setImageBase64(reader.result);
       };
       reader.readAsDataURL(file);
     }
@@ -76,8 +76,25 @@ export default function Chatbox() {
           <p className="text-sm text-green-400">Hii, I am using Convo</p>
         </div>
         <div className="flex flex-row space-x-4">
-          <img src={phone} height={40} width={40} className="cursor-pointer" title="Voice Call" alt="Phone Call" />
-          <img src={video} height={40} width={40} className="cursor-pointer" title="Video Call" alt="Video Call" />
+          <img
+            src={phone}
+            height={40}
+            width={40}
+            className="cursor-pointer"
+            title="Voice Call"
+            alt="Phone Call"
+            onClick={() => startCall("voice")}
+          />
+
+          <img
+            src={video}
+            height={40}
+            width={40}
+            className="cursor-pointer"
+            title="Video Call"
+            alt="Video Call"
+            onClick={() => startCall("video")}
+          />
         </div>
       </div>
       <hr className="border-gray-500" />
@@ -86,22 +103,22 @@ export default function Chatbox() {
         <div className="messages">
           {Chat?.messages?.map((message, idx) => (
             <div key={idx} className={`w-[100%] mb-2 flex ${message.senderId === currentUser.id
-                  ? "flex-row-reverse text-right"
-                  : "flex-row text-left"
-                }`}>
+              ? "flex-row-reverse text-right"
+              : "flex-row text-left"
+              }`}>
               <div className="message-bubble max-w-[70%]">
                 {message.text && (
                   <p className={`p-3 rounded-lg ${message.senderId === currentUser.id
-                        ? "bg-blue-600 mt-2 text-white"
-                        : "bg-gray-600 text-white mt-2"
-                      }`}>
+                    ? "bg-blue-600 mt-2 text-white"
+                    : "bg-gray-600 text-white mt-2"
+                    }`}>
                     {message.text}
                   </p>
                 )}
                 {message.imageBase64 && (
                   <img src={message.imageBase64} alt="Sent"
                     className={`w-auto cursor-pointer h-auto max-w-[200px] mt-2 rounded-lg ${message.senderId === currentUser.id ? "float-right" : "float-left"
-                    }`}/>
+                      }`} />
                 )}
               </div>
             </div>
@@ -113,24 +130,24 @@ export default function Chatbox() {
       <div className="input-area mt-4 flex flex-col space-y-2">
         {imageBase64 && (
           <div className="image-preview p-2 border border-gray-700 rounded-lg flex justify-between items-center">
-            <img src={imageBase64} alt="Preview" className="max-w-[200px] max-h-[200px] rounded-lg"/>
-            <button onClick={() => setImageBase64(null)}  className="text-red-500 font-bold">
+            <img src={imageBase64} alt="Preview" className="max-w-[200px] max-h-[200px] rounded-lg" />
+            <button onClick={() => setImageBase64(null)} className="text-red-500 font-bold">
               Cancel
             </button>
           </div>
         )}
         {/* Add Image,mic,text,emoji Button */}
         <div className="flex items-center space-x-2">
-          <input type="file" onChange={handleImageChange} className="hidden" id="image-upload"/>
+          <input type="file" onChange={handleImageChange} className="hidden" id="image-upload" />
           <label htmlFor="image-upload">
             <img src={addimg} height={30} width={30} className="cursor-pointer" alt="Add Image" />
           </label>
           <img src={mic} height={30} width={30} className="cursor-pointer" alt="Microphone" />
           <input type="text" value={text} onChange={(e) => setText(e.target.value)}
-          className="bg-gray-800 h-[40px] flex-grow rounded-lg focus:outline-none text-white px-4" placeholder="Type a message..." 
-          disabled={isCurrentUserBlocked || isRecieverBlocked}/>
+            className="bg-gray-800 h-[40px] flex-grow rounded-lg focus:outline-none text-white px-4" placeholder="Type a message..."
+            disabled={isCurrentUserBlocked || isRecieverBlocked} />
           <div className="relative">
-            <img src={emojiIcon} height={30} width={30} className="cursor-pointer" alt="Emoji" onClick={() => setOpen((prev) => !prev)}/>
+            <img src={emojiIcon} height={30} width={30} className="cursor-pointer" alt="Emoji" onClick={() => setOpen((prev) => !prev)} />
             {open && (
               <div className="absolute bottom-10 right-0 z-50">
                 <EmojiPicker onEmojiClick={handleEmoji} />
@@ -139,7 +156,7 @@ export default function Chatbox() {
           </div>
           {/* Send Button */}
           <button onClick={handleSend}
-          className="bg-blue-500 text-white h-[40px] px-4 rounded-lg hover:bg-blue-600" disabled={isCurrentUserBlocked || isRecieverBlocked}>
+            className="bg-blue-500 text-white h-[40px] px-4 rounded-lg hover:bg-blue-600" disabled={isCurrentUserBlocked || isRecieverBlocked}>
             Send
           </button>
         </div>
