a4e1e99173e5f3f1d66ad3e6e04a5723b2c88daa
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-10 17:36:03 +0530

    isfgkffh

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -11,6 +11,8 @@ import { db } from "../db";  // Firebase Database
 export default function Detail() {
   const { chatId, user } = Chatstore(); // Chat Management
   const [images, setImages] = useState([]); // State to store sent images
+  const [selectedImage, setSelectedImage] = useState(null); // State to store selected image
+
 
   // Storing the messages in chats collection in db if chat id and user id is present.
   useEffect(() => {
