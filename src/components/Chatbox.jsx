8f3c566c7f343cc34102f1ccff7aa31a16aafd15
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-10 16:56:47 +0530

    iscoi=

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -37,7 +37,6 @@ export default function Chatbox() {
     const data = new FormData();
     data.append("file", file);
     data.append("upload_preset", "chat_app");
-    data.append("cloud_name", "dhszvdzft");
 
     const res = await fetch(
       "https://api.cloudinary.com/v1_1/dhszvdzft/image/upload",
